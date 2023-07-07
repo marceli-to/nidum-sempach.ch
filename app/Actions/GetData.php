d910ed45f6b9a8ba4c93bb2f394d4b3aa4be93aa
Marcel Stadelmann <m@marceli.to>
2023-07-07 13:37:02 +0200

    wip

--- app/Actions/GetData.php
+++ app/Actions/GetData.php
@@ -6,11 +6,16 @@ use Illuminate\Support\Facades\Storage;
 class GetData
 {
   public function execute()
-  {
-    if (!Storage::disk('public')->exists('apartements.json'))
+  { 
+    // if there is no file or the existing file is older than 60 minutes, fetch new data
+    if (
+      !Storage::disk('public')->exists('apartements.json') || 
+      Storage::disk('public')->lastModified('apartements.json') < now()->subMinutes(60)->timestamp
+    )
     {
       (new FetchData)->execute();
     }
+
     $data = Storage::disk('public')->get('apartements.json');
     return collect(json_decode($data, true))->sortBy('title'); 
   }
